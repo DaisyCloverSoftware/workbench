28522b9efc6351c9ef319b6ff5dfa0d155aabe5f
mattbrownsett <33632646+mattbrownsett@users.noreply.github.com>
2026-08-17 01:21:35 +0100

    release: bump relay version to 0.9.4

--- cmd/workbench-relay/main.go
+++ cmd/workbench-relay/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/DaisyCloverSoftware/workbench/internal/core"
 )
 
-const relayVersion = "0.9.3"
+const relayVersion = "0.9.4"
 
 type envelope struct {
 	Version   int    `json:"version"`
