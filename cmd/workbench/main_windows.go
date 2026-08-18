498044b3e3f8422d28b334299941a6c9c75a9ff8
Workbench Release Automation <workbench-release@users.noreply.github.com>
2026-08-18 21:47:11 +0000

    release: Workbench 0.9.21

--- cmd/workbench/main_windows.go
+++ cmd/workbench/main_windows.go
@@ -8,7 +8,7 @@ import (
 	"github.com/DaisyCloverSoftware/workbench/internal/desktop"
 )
 
-const appVersion = "0.9.20"
+const appVersion = "0.9.21"
 
 func main() {
 	if workbenchSingleInstanceHandle == 0 {
