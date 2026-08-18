c9d87b1b7fad0411302782ca46e1439805db9a7d
Workbench Release Automation <workbench-release@users.noreply.github.com>
2026-08-18 14:44:16 +0000

    release: Workbench 0.9.12

--- cmd/workbench-relay/main.go
+++ cmd/workbench-relay/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/DaisyCloverSoftware/workbench/internal/core"
 )
 
-const relayVersion = "0.9.11"
+const relayVersion = "0.9.12"
 
 type envelope struct {
 	Version   int    `json:"version"`
--- cmd/workbench/main_windows.go
+++ cmd/workbench/main_windows.go
@@ -8,7 +8,7 @@ import (
 	"github.com/DaisyCloverSoftware/workbench/internal/desktop"
 )
 
-const appVersion = "0.9.11"
+const appVersion = "0.9.12"
 
 func main() {
 	if workbenchSingleInstanceHandle == 0 {
