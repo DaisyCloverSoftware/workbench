a36c82dbae6da9c40d3fb1b36b4650128ade0198
Workbench Release Automation <workbench-release@users.noreply.github.com>
2026-08-19 14:38:06 +0000

    release: Workbench 0.9.27

--- cmd/workbench-relay/main.go
+++ cmd/workbench-relay/main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/DaisyCloverSoftware/workbench/internal/core"
 )
 
-const relayVersion = "0.9.26"
+const relayVersion = "0.9.27"
 
 type envelope struct {
 	Version   int    `json:"version"`
